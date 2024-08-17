20cbf829d9fa1bbf2db046e871426f606478cb05
vojtikczhraje <vojta271000@gmail.com>
2024-08-17 21:44:58 +0200

    add: navbar opening

--- app/components/navbar.tsx
+++ app/components/navbar.tsx
@@ -6,14 +6,34 @@ const Navbar = () => {
     const [isOpen, setIsOpen] = useState(false)
 
   return (
-    <div className='w-full fixed top-0 py-[3.8vh] px-[1.8vh]  sm:py-[3.78vh] sm:px-[7.77vh] flex items-center bor'>
-        <div className='text-[2vh] sm:text-[2.4vh]'>vojtikczhraje</div>
+    <div className='w-full fixed top-0 py-[3.8vh] px-[1.8vh]  sm:py-[3.78vh] sm:px-[7.77vh] flex items-center bor z-[100]'>
+      <div className={`${isOpen ? 'translate-y-0' : 'translate-y-[-100%]'} duration-1000 transition-all ease-in-out fixed h-full w-full sm:w-[50vh] bg-black top-0 right-0`}>nice cock</div>
 
-        <div onClick={() => setIsOpen(!isOpen)} className={`${isOpen ? '' : ''} flex flex-col gap-[1vh] ml-auto cursor-pointer transition-all duration-300 ease-in-out`}>
-            <div className={`${isOpen ? 'rotate-[45deg] sm:translate-y-[15px] translate-y-[12px]' : ''} w-[6vh] sm:w-[9vh] h-[0.2vh] bg-white transition-all duration-300 ease-in-out`}></div>
-            
-            <div className={`${isOpen ? 'rotate-[-45deg]  ' : ''} w-[6vh] sm:w-[9vh] h-[0.2vh] bg-white transition-all duration-300 ease-in-out`}></div>
-        </div>
+      <div className='text-[2vh] sm:text-[2.4vh]'>vojtikczhraje</div>
+      
+      <div
+        onClick={() => setIsOpen(!isOpen)}
+        className={`${
+          isOpen ? '' : ''
+        } flex flex-col gap-[1vh] ml-auto cursor-pointer transition-all  duration-300 ease-in-out z-[50]`}
+      >
+        <div
+          className={`${
+            isOpen
+              ? 'rotate-[45deg] translate-y-[4px]'
+              : ''
+          } w-[6vh] sm:w-[9vh] h-[0.2vh] bg-white transition-all duration-300 ease-in-out`}
+        ></div>
+
+
+        <div
+          className={`${
+            isOpen
+              ? 'rotate-[-45deg] translate-y-[-8px]'
+              : ''
+          } w-[6vh] sm:w-[9vh] h-[0.2vh] bg-white transition-all duration-300 ease-in-out`}
+        ></div>
+      </div>
     </div>
   )
 }
---
+++ app/loading.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const loading = () => {
+  return (
+    <div>Nice cock</div>
+  )
+}
+
+export default loading

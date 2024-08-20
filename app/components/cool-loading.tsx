4b0c0e8a0f8d0f25d335d516696b594c49cfc30b
vojtikczhraje <vojta271000@gmail.com>
2024-08-20 13:19:06 +0200

    i'm loosing it

--- app/components/cool-loading.tsx
+++ app/components/cool-loading.tsx
@@ -1,9 +1,11 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react';
 
 const Loading = () => {
-    const [currentLang, setCurrentLang] = useState(0);
+  const [currentIndex, setCurrentIndex] = useState(0);
+  const [nothing, setNothingToLoad] = useState(false)
+  const [loadingBar, setLoadingBar] = useState(0);
 
-    const loadingTranslations: string[] = [
+  const loadingTranslations: string[] = [
         "Loading",
         "ローディング", // Japanese
         "Načítání",               // Czech
@@ -13,21 +15,50 @@ const Loading = () => {
         "加载中",  // Chinese (Simplified)
         "Загрузка",    // Russian    // Italian
         "Carregando",             // Portuguese
-        "로딩 중"     // Korean
+        "로딩 중",     // Korean
+        "Wait, but there is nothing to load"     // Korean
       ];
 
-      for (let i = 0; i <= 9; i++) {
-        setTimeout(() => {
-            if (i <= 9) { // Condition to stop execution
-                setCurrentLang(currentLang + i);
-                console.log(i);
+      useEffect(() => {
+        const intervalId = setInterval(() => {
+          setCurrentIndex((prevIndex) => {
+            if (prevIndex < 9) {
+              return prevIndex + 1;
+            } else {
+              clearInterval(intervalId);
+              return prevIndex;
             }
-        }, 1000 * i);
-    }
+          });
+        }, 300);
 
-  return (
-    <div className='fixed top-0 w-full h-full flex justify-center items-center z-[100] bg-[#0C0C14] text-[10vh] italic'>{currentLang}</div>
-  )
-}
+        
+    
+        return () => clearInterval(intervalId);
+      }, []);
 
-export default Loading
+
+      if(currentIndex === 9) {
+        setTimeout(() => {
+          setCurrentIndex(10)
+        }, 3000)
+      }
+      
+      
+
+      
+      console.log(currentIndex)
+    
+      return (
+        <div className='fixed top-0 w-full h-full flex flex-col gap-4 justify-center items-center z-[100] bg-[#0C0C14] text-[8vh] italic'>
+          <div className=''>{loadingTranslations[currentIndex]}</div>
+          <div className='relative w-[50vh] bg-[#444C64]  h-[10px] overflow-hidden w-fit'>
+          <div
+            className='absolute top-0 left-0 h-full w-full  transition-all duration-[0.3s] bg-[#D9D9D9]'
+            style={{ transform: `translateX(${(currentIndex + 1) * 5}%)` }}
+          />
+      </div>
+        </div>
+      );
+    };
+    
+    export default Loading;
